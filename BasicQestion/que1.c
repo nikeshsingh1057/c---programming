fd5a5acbf2d26518d45a6f5e92304d42f4af783b
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-06-21 14:07:45 +0530

    find the area of rectangle

---
+++ BasicQestion/que1.c
@@ -0,0 +1,11 @@
+//find the area of rectangle?
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("length of rectangle:\n");
+    scanf("%d", &a);
+    printf("breath of rectangle:\n");
+    scanf("%d", &b);
+    printf("area of rectangle:\n%d", a * b);
+}
