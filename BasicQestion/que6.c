e40c7a6adeb3c3f9facf8a9cf0808f0c87894a8e
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-06-21 14:11:49 +0530

    convert meter into km

---
+++ BasicQestion/que6.c
@@ -0,0 +1,13 @@
+//convert meter into km?
+
+#include <stdio.h>
+int main()
+{
+ 
+    float a;
+    printf("value in meter:");
+    scanf("%f", &a);
+
+    printf("value in km:%f",a /1000);
+    return 0;
+}
