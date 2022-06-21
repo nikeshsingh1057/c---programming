69ce969c21750f18bac7daf32227103e9b9bd9e3
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-06-21 14:25:04 +0530

    Create sir7.c

---
+++ BasicQestion/sir7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    float a;
+    printf("raju got equally apple 6 and half from each of  raghu,sheenu ,akash"); //6 and half mean=6.5 apple
+        a = 6.5 * 3;                     // a means total apple
+    printf("total apple he got: %.2f",a);
+}
