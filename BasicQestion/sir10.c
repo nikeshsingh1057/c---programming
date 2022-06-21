0788d9b58b49078c6d1e0b0d1d5cc002d060d117
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-06-21 14:27:53 +0530

    population of people

---
+++ BasicQestion/sir10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float p=30000, p1,p2;
+//float m=20,n=30; 
+printf("population of city is p = 30000\n");
+p1=(20.0/100)*p+p;
+printf("population of people increase in in 1st year:%f\n",p1);
+p2=(30.0/100)*p1+p1;
+printf("population of people increase in in two year:%f\n",p2);
+
+}
