62b6bd4cde6ec6c14cd4fab9b47211d565fe1e42
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-06-21 13:59:05 +0530

    Add files via upload

---
+++ If else and Switch case/ifelse2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+    {
+        int a;
+        printf("enter no:");
+        scanf("%d",&a);
+        if(a%5==0 && a%11==0)
+        {
+        printf("%d is disivle by 5 and 11",a);
+        }
+       else
+        printf("not disivle");
+        
+   
+
+    }
