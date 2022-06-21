0539198a52c30894dde8d466274b4771973c30df
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-06-21 14:29:43 +0530

    programme to find ASCII value of character

---
+++ BasicQestion/sir11.c
@@ -0,0 +1,9 @@
+// programme to find ASCII value of character
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter character value:");
+scanf("%c",&ch);
+printf("ASCII value of character:%d",ch);
+}
